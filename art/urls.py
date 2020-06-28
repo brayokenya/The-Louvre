62bda3e8d237a9b33d14f7a46e5adc6238c8c451
brayokenya <kiirubrian21@gmail.com>
2020-06-28 22:16:59 +0300

    re defined models

--- art/urls.py
+++ art/urls.py
@@ -6,6 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.art_of_day,name='artToday'),
     url(r'^search/', views.search_results, name='search_results'), 
+    url(r'^location/(\d+)',views.location,name = 'location'),
+
 
 ]
 if settings.DEBUG:
